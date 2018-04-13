bed22ea431064f224773fb6fe0b0433e24604a68
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2018-04-13 14:00:56 +0800

    add ci (for *.md)

--- ci/ci.py
+++ ci/ci.py
@@ -1,31 +1,101 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 09:35:57 2018
+@author: JianKai Wang
+"""
 
-from os import listdir
-from os.path import isfile, join
+import os
+from os.path import isfile, join, basename, splitext
+from pathlib import Path
+import sys
+import getopt
+import logging
 import subprocess
-import re
 
-mypath = "../data"
+# help message
+def helpMessgae():
+    print("""Usage: python ci.py [options]
+[options]
+-e              the path of sophia project (necessary)
+-i              input directory path (necessary)
+-o              output directory path (necessary)
+-h, --help      the help message
+    """)
 
-# *.rmd and *.md
-def checkFileExt(name):
-    if "md" in name:
-        return True
+# parse opts
+def parseOpts(get_opts):
+    return(dict((k,v) for k,v in get_opts))
+    
+# check file path
+def checkFInOut(get_opts):
+    if '-e' in opts.keys() and '-i' in opts.keys() and '-o' in opts.keys():
+        logging.debug(opts['-i'])
+        if Path(opts['-e']).is_dir() and Path(opts['-i']).is_dir() and Path(opts['-o']).is_dir():
+            return 1, get_opts['-e'], get_opts['-i'], get_opts['-o']
+    logging.debug("not existing parameter")
+    return -1, "", "", ""
+
+# convert from *.md to *.html
+def getConvertList(fin, fout):
+    onlyfiles = {}
+    whiteList = ["model.md","framework.md","maker.md","uitools.md","statistics.md"]
+    
+    for f in os.listdir(fin):
+        filepath = join(fin, f)
+        if isfile(filepath) and filepath.lower().endswith(('.md',)) and f not in whiteList:
+            name = splitext(basename(filepath))[0]
+            onlyfiles.setdefault(f,name + ".html")
+    
+    return onlyfiles
+    
+# 
+def convertToHtml(fexec, fin, fout, getfiles):
+    templateHtml = join(fexec,"ci","template","default.html")
+    headerHtml = join(fexec,"ci","template","header.html")
+    
+    for all_md in getfiles.keys():
+        bashCommand = """pandoc +RTS -K512m -RTS {} --to html4 --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash+smart --output {} --email-obfuscation none --self-contained --standalone --section-divs --template {} --no-highlight --variable highlightjs=1 --variable "theme:bootstrap" --mathjax --variable "mathjax-url:https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" --include-in-header {} """.format(join(fin,all_md), join(fin,getfiles[all_md]), templateHtml, headerHtml)
+         
+        logging.debug(bashCommand)
+        try:        
+            process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output, error = process.communicate()
+            print('Output:{},Error:{}'.format(output.decode('utf-8'), error.decode('utf-8')))
+        except:
+            print("Failure to convert markdown {}.".format(all_md))
+    
+    
+# main entry
+logging.basicConfig(level=logging.WARNING)
+
+opts, args = getopt.getopt(sys.argv[1:], "fhe:i:o:", ["help"])
+opts = parseOpts(opts)
+
+if len(opts) < 1:
+    logging.debug("opts < 1")
+    helpMessgae()
+elif '--help' in opts.keys() or '-h' in opts.keys():
+    logging.debug("opts exists help")
+    helpMessgae()
+else:
+    logging.debug("others")
+    flag, fexec, fin, fout = checkFInOut(opts)
+    os.chdir(fin)
+    if flag != 1:
+        print("Error: Parameter is not valid.")
+        helpMessgae()
     else:
-        return False
-def transform2html(file_name):
-    bashCommand = 'Rscript ./gmd.r {}'.format(mypath + "/" + file_name)
-    try:        
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        searchObj = re.search(r'Error', output.decode('utf-8'), re.M | re.I)
-        if len(searchObj.group()) > 4:
-            return False
-        else:
-            return True
-    except:
-        return False
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and checkFileExt(join(mypath, f))]
-for item in onlyfiles:
-    if not transform2html(item):
-        print("Error in rendering file " + item)
+        logging.debug("precheck ok")
+        fileNameList = getConvertList(fin, fout)
+        convertToHtml(fexec, fin, fout, fileNameList)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
